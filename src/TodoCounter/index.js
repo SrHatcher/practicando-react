f16e11c147f30d7a572c801edf150c1312aacaca
SrHatcher <erickomaldonado11@gmail.com>
2022-08-19 11:13:14 -0600

    clase para hacer deploy en github pages

--- src/TodoCounter/index.js
+++ src/TodoCounter/index.js
@@ -5,7 +5,7 @@ import {TodoContext} from '../todoContext'
 function TodoCounter(){
     const {totalTodos, completedTodos}=useContext(TodoContext)
     return (
-        <h2 className='TodoCounter'>Has completado {completedTodos} de {totalTodos} TODOs</h2>
+        <h2 className='TodoCounter'>Has completado {completedTodos} de {totalTodos} Tareas</h2>
     )
 }
 
